1a1bca76c141ba7805e7d80f3d8101cec298d0d2
Sofia Sandoval <sandovalsofi99@gmail.com>
2024-04-09 19:52:36 -0300

    3)

--- tp2_3.c
+++ tp2_3.c
@@ -1,16 +1,19 @@
 #define N 5
 #define M 7
+#include <stdio.h>
 
 void main(){
 
 int i,j;
 int mt[N][M];
+int* pMt = mt;
 for(i = 0;i<N; i++)
     {
     for(j = 0;j<M; j++)
         {
-        mt[i][j]=1+rand()%100;
-        printf("%lf", mt[i][j]);
+        *pMt=1+rand()%100;
+        printf("%d ", *pMt);
+        pMt++;
         }
     printf("\n");
     }
